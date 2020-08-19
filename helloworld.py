e86e23b67af6a50b01eb7d91c0d58bcbb5b22772
wqk <553681001@qq.com>
2020-08-19 16:11:53 +0800

    version 1.2

--- helloworld.py
+++ helloworld.py
@@ -4,4 +4,5 @@ a = np.array([1,1,1])
 b = np.array([2,2,2])
 
 print('hello world ' + str(np.sum(np.dot(a, b))) + ' times !')
-print('hello '+'hello')
+print('hello '+'hello'+' hello')
+print('hello world !')
